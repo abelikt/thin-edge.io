1902bc123e56e563ced45c65e7f7ab19c12c32eb
Michael Abel <info@abel-ikt.de>
2021-06-21 09:12:05 +0200

    Move testbed code

--- tests/PySys/mapper_testbed/c8y_positive/run.py
+++ tests/PySys/mapper_testbed/c8y_positive/run.py
@@ -1,8 +1,10 @@
 from pysys.basetest import BaseTest
-
-import time
-import json
 import jsonschema
+
+import sys
+sys.path.append("mapper_testbed")
+from mapper_testbed_c8y import MapperTestbedC8y
+
 
 """
 Validate a tedge-mapper-c8y message that is published
@@ -18,160 +20,7 @@ Then we stop the tedge-mapper-c8y systemctl service
 
 """
 
-# TODO : publish and subscribe with mosquitto clients ?
-# TODO Make more generic c8y, az, collectd
-# TODO Subscribe with mosquitto client with timeout -> avoid kill
-# TODO How to wait for collectd ?
-# TODO How to specify unprecise e.g. with ANY
-
-# Handcrafted schema just for this test case:
-tedgeschema = {
-    "$schema": "https://json-schema.org/draft/2020-12/schema",
-    "$id": "https://example.com/product.schema.json",
-    "title": "ThinEdgeMeasurement",
-    "description": "A thin edge measurement",
-    "type": "object",
-    "properties": {
-        "type": {"description": "The type", "type": "string"},
-        "temperature": {
-            "description": "The temperature",
-            "type": "object",
-            "properties": {
-                "type": {
-                    "temperature": "The type",
-                    "type": "object",
-                    "properties": {
-                        "type": {"value": "The type", "type": "integer"},
-                    },
-                    "required": ["value"],
-                },
-            },
-            "required": ["temperature"],
-        },
-        "time": {"description": "The time", "type": "string"},
-    },
-    "required": ["type", "temperature", "time"],
-}
-
-
-class TedgeMapperC8yBed(BaseTest):
-    def setup(self):
-        self.tedge = "/usr/bin/tedge"
-        self.sudo = "/usr/bin/sudo"
-
-        self.tedge = "/usr/bin/tedge"
-        self.tedge_mapper_c8y = "tedge-mapper-c8y"
-        self.tedge_mapper_az = "tedge-mapper-az"
-        self.sudo = "/usr/bin/sudo"
-        self.systemctl = "/usr/bin/systemctl"
-
-        # Check if tedge-mapper is in disabled state
-        serv_mapper = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", "collectd-mapper"],
-            stdouterr="serv_mapper1",
-            expectedExitStatus="==3",  # 3: disabled
-        )
-
-        serv_mapper = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", self.tedge_mapper_c8y],
-            stdouterr="serv_mapper1",
-            expectedExitStatus="==3",  # 3: disabled
-        )
-
-        serv_mapper = self.startProcess(
-            command=self.systemctl,
-            arguments=["status", self.tedge_mapper_az],
-            stdouterr="serv_mapper1",
-            expectedExitStatus="==3",  # 3: disabled
-        )
-
-        mapper = self.startProcess(
-            command=self.sudo,
-            arguments=["systemctl", "start", "tedge-mapper-c8y"],
-            stdouterr="tedge_mapper_c8y",
-        )
-
-        self.addCleanupFunction(self.mapper_cleanup)
-
-    def execute(self):
-        sub = self.startProcess(
-            command=self.sudo,
-            arguments=[
-                self.tedge,
-                "mqtt",
-                "sub",
-                "--no-topic",
-                "c8y/measurement/measurements/create",
-            ],
-            stdouterr="tedge_sub",
-            background=True,
-        )
-
-        sub_errror = self.startProcess(
-            command=self.sudo,
-            arguments=[self.tedge, "mqtt", "sub", "--no-topic", "tedge/errors"],
-            stdouterr="tedge_sub_error",
-            background=True,
-        )
-
-        # Wait for a small amount of time to give tedge sub time
-        # to initialize. This is a heuristic measure.
-        # Without an additional wait we observe failures in 1% of the test
-        # runs.
-        time.sleep(0.1)
-
-        pub = self.startProcess(
-            command=self.sudo,
-            arguments=[self.tedge, "mqtt", "pub", "tedge/measurements", self.message],
-            stdouterr="tedge_temp",
-        )
-
-        # Kill the subscriber process explicitly with sudo as PySys does
-        # not have the rights to do it
-        kill = self.startProcess(
-            command=self.sudo,
-            arguments=["killall", "tedge"],
-            stdouterr="kill_out",
-        )
-
-    def validate(self):
-        f = open(self.output + "/tedge_sub.out", "r")
-        data = f.read()
-        self.log.info(data)
-        self.c8y_json = json.loads(data)
-        self.log.info(self.c8y_json)
-
-        f = open(self.output + "/tedge_sub_error.out", "r")
-        data = f.read()
-        self.log.info(data)
-        if data:
-            self.errors = json.loads(data)
-            self.log.info(self.errors)
-        else:
-            self.errors = None
-            self.log.info("No errors")
-
-    def mapper_cleanup(self):
-        self.log.info("mapper_cleanup")
-        mapper = self.startProcess(
-            command=self.sudo,
-            arguments=["systemctl", "stop", "tedge-mapper-c8y"],
-            stdouterr="tedge_mapper_c8y",
-        )
-
-    def assert_json_key(self, key, value):
-        self.assertThat("actual == expected", actual=self.c8y_json[key], expected=value)
-
-    def assert_json(self, key, value):
-        self.assertThat("actual == expected", actual=key, expected=value)
-
-    def assert_no_error(self):
-        self.assertThat("actual == expected", actual=self.errors, expected=None)
-
-
-class TedgeMapperC8y(TedgeMapperC8yBed):
+class TedgeMapperC8y(MapperTestbedC8y):
     def setup(self):
         super().setup()
         self.message = (
@@ -196,7 +45,7 @@ class TedgeMapperC8y(TedgeMapperC8yBed):
         self.assert_json_key("time", "2021-06-15T17:01:15.806181503+02:00")
         self.assert_no_error()
 
-        jsonschema.validate(instance=self.c8y_json, schema=tedgeschema)
+        jsonschema.validate(instance=self.c8y_json, schema=self.tedgeschema)
 
         # also possible
         assert self.c8y_json == self.expect
---
+++ tests/PySys/mapper_testbed/mapper_testbed_c8y.py
@@ -0,0 +1,162 @@
+
+import json
+import sys
+import time
+import jsonschema
+from pysys.basetest import BaseTest
+
+
+# TODO : publish and subscribe with mosquitto clients ?
+# TODO Make more generic c8y, az, collectd
+# TODO Subscribe with mosquitto client with timeout -> avoid kill
+# TODO How to wait for collectd ?
+# TODO How to specify unprecise e.g. with ANY
+
+
+class MapperTestbedC8y(BaseTest):
+    # Handcrafted JSON schema just for this test case:
+    # Will check for this pattern
+    # {'type': 'ThinEdgeMeasurement', 'temperature': {'temperature': {'value': 12}}, 'time': '2021-06-15T17:01:15.806181503+02:00'}
+
+    tedgeschema = {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "$id": "https://example.com/product.schema.json",
+        "title": "ThinEdgeMeasurement",
+        "description": "A thin edge measurement",
+        "type": "object",
+        "properties": {
+            "type": {"description": "The type", "type": "string"},
+            "temperature": {
+                "description": "The temperature",
+                "type": "object",
+                "properties": {
+                    "type": {
+                        "temperature": "The type",
+                        "type": "object",
+                        "properties": {
+                            "type": {"value": "The type", "type": "integer"},
+                        },
+                        "required": ["value"],
+                    },
+                },
+                "required": ["temperature"],
+            },
+            "time": {"description": "The time", "type": "string"},
+        },
+        "required": ["type", "temperature", "time"],
+    }
+
+    def setup(self):
+        self.tedge = "/usr/bin/tedge"
+        self.sudo = "/usr/bin/sudo"
+
+        self.tedge = "/usr/bin/tedge"
+        self.tedge_mapper_c8y = "tedge-mapper-c8y"
+        self.tedge_mapper_az = "tedge-mapper-az"
+        self.sudo = "/usr/bin/sudo"
+        self.systemctl = "/usr/bin/systemctl"
+
+        # Check if tedge-mapper is in disabled state
+        serv_mapper = self.startProcess(
+            command=self.systemctl,
+            arguments=["status", "collectd-mapper"],
+            stdouterr="serv_mapper1",
+            expectedExitStatus="==3",  # 3: disabled
+        )
+
+        serv_mapper = self.startProcess(
+            command=self.systemctl,
+            arguments=["status", self.tedge_mapper_c8y],
+            stdouterr="serv_mapper1",
+            expectedExitStatus="==3",  # 3: disabled
+        )
+
+        serv_mapper = self.startProcess(
+            command=self.systemctl,
+            arguments=["status", self.tedge_mapper_az],
+            stdouterr="serv_mapper1",
+            expectedExitStatus="==3",  # 3: disabled
+        )
+
+        mapper = self.startProcess(
+            command=self.sudo,
+            arguments=["systemctl", "start", "tedge-mapper-c8y"],
+            stdouterr="tedge_mapper_c8y",
+        )
+
+        self.addCleanupFunction(self.mapper_cleanup)
+
+    def execute(self):
+        sub = self.startProcess(
+            command=self.sudo,
+            arguments=[
+                self.tedge,
+                "mqtt",
+                "sub",
+                "--no-topic",
+                "c8y/measurement/measurements/create",
+            ],
+            stdouterr="tedge_sub",
+            background=True,
+        )
+
+        sub_errror = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "mqtt", "sub", "--no-topic", "tedge/errors"],
+            stdouterr="tedge_sub_error",
+            background=True,
+        )
+
+        # Wait for a small amount of time to give tedge sub time
+        # to initialize. This is a heuristic measure.
+        # Without an additional wait we observe failures in 1% of the test
+        # runs.
+        time.sleep(0.1)
+
+        pub = self.startProcess(
+            command=self.sudo,
+            arguments=[self.tedge, "mqtt", "pub", "tedge/measurements", self.message],
+            stdouterr="tedge_temp",
+        )
+
+        # Kill the subscriber process explicitly with sudo as PySys does
+        # not have the rights to do it
+        kill = self.startProcess(
+            command=self.sudo,
+            arguments=["killall", "tedge"],
+            stdouterr="kill_out",
+        )
+
+    def validate(self):
+        f = open(self.output + "/tedge_sub.out", "r")
+        data = f.read()
+        self.log.info(data)
+        self.c8y_json = json.loads(data)
+        self.log.info(self.c8y_json)
+
+        f = open(self.output + "/tedge_sub_error.out", "r")
+        data = f.read()
+        self.log.info(data)
+        if data:
+            self.errors = json.loads(data)
+            self.log.info(self.errors)
+        else:
+            self.errors = None
+            self.log.info("No errors")
+
+    def mapper_cleanup(self):
+        self.log.info("mapper_cleanup")
+        mapper = self.startProcess(
+            command=self.sudo,
+            arguments=["systemctl", "stop", "tedge-mapper-c8y"],
+            stdouterr="tedge_mapper_c8y",
+        )
+
+    def assert_json_key(self, key, value):
+        self.assertThat("actual == expected", actual=self.c8y_json[key], expected=value)
+
+    def assert_json(self, key, value):
+        self.assertThat("actual == expected", actual=key, expected=value)
+
+    def assert_no_error(self):
+        self.assertThat("actual == expected", actual=self.errors, expected=None)
